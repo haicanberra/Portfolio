b4de0d70bcb4c61ac2ae06a4008dee8953e4f7de
Hai Hoang <haicanberra@gmail.com>
2023-06-08 03:03:24 -0600

    Test JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,43 @@
-// URL of the GitHub API to fetch the repositories
-const apiUrl = 'https://api.github.com/users/haicanberra/repos';
+const user = "haicanberra";
+const topic = "project";
+const apiUrl = `https://api.github.com/users/${user}/repos`;
 
-// Fetch repositories from the GitHub API
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        const reposList = document.getElementById('repos-list');
+        const repos = data.filter(repo => repo.topics.includes(topic));
 
-        // Filter repositories based on the tag
-        const filteredRepos = data.filter(repo => {
-            const tags = repo.topics || [];
-            return tags.includes('project');
+        let htmlSnippet = "";
+        repos.forEach(repo => {
+            const repoTitle = repo.name;
+            const repoDescription = repo.description ? repo.description.replace(/:white_check_mark:/g, "✅") : "";
+            const repoLink = repo.html_url;
+            const thumbnailUrl = `${repo.html_url}/raw/master/thumbnail.png`;
+
+            const repoHtml = `
+        <div class="cell col-sm-6 col-md-4 col-lg-4 mb-4">
+          <a href="${repoLink}" target="_blank" class="cell-wrap">
+            <div class="project-info">
+              <h2>${repoTitle}</h2>
+              <span>${repoDescription}</span>
+            </div>
+            <img class="img-fluid" src="${thumbnailUrl}">
+          </a>
+        </div>
+      `;
+
+            htmlSnippet += repoHtml;
         });
 
-        // Iterate over the filtered repositories and create list items
-        filteredRepos.forEach(repo => {
-            const listItem = document.createElement('li');
-            const link = document.createElement('a');
-            const description = document.createElement('p');
-
-            // Set the link's text, href, and target attributes
-            link.textContent = repo.name;
-            link.href = repo.html_url;
-            link.target = '_blank';
-
-            // Check if the repository description contains :white_check_mark:
-            if (repo.description.includes(':white_check_mark:')) {
-                // Create <g-emoji> element for :white_check_mark: emoji
-                const emojiElement = document.createElement('g-emoji');
-                emojiElement.setAttribute('class', 'g-emoji');
-                emojiElement.setAttribute('alias', 'white_check_mark');
-                emojiElement.setAttribute('fallback-src', 'https://github.githubassets.com/images/icons/emoji/unicode/2705.png');
-                emojiElement.textContent = '✅';
-
-                // Set the repository description
-                description.appendChild(emojiElement);
-
-                // Remove :white_check_mark: from the description
-                repo.description = repo.description.replace(/:white_check_mark:/g, '');
-            }
-
-            description.innerHTML += repo.description;
-
-            listItem.appendChild(link);
-            listItem.appendChild(description);
-            reposList.appendChild(listItem);
-        });
+        const sectionContainer = document.querySelector('.section .container');
+        const existingRow = sectionContainer.querySelector('.row.no-gutter');
+        const newRow = document.createElement('div');
+        newRow.classList.add('row', 'no-gutter');
+        newRow.setAttribute('data-aos', 'fade-up');
+        newRow.setAttribute('data-aos-delay', '200');
+        newRow.innerHTML = htmlSnippet;
+        sectionContainer.replaceChild(newRow, existingRow);
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+        console.error("Error fetching repositories:", error);
+    });
